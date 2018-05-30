c3157a008b90ead8e21ef359ca1287284fb21e7b
Matthew Marine <matthew.marine@bluequartz.net>
2018-05-30 16:46:21 -0400

    Merge branch 'feature/VisualizationTab' into feature/UX_StyleSheet
    # Conflicts:
    #	Source/SIMPLView/SIMPLView_UI.cpp
    #	Source/SIMPLView/UI_Files/SIMPLView_UI.ui

--- Source/SIMPLView/SIMPLView_UI.h
+++ Source/SIMPLView/SIMPLView_UI.h
@@ -137,6 +137,16 @@ class SIMPLView_UI : public QMainWindow
      */
     void executePipeline();
 
+    /**
+     * @brief Selects the FilterInputWidget tab
+     */
+    void showFilterParameterTab();
+
+    /**
+     * @brief Selects the vizualization tab
+     */
+    void showVisualizationTab();
+
   public slots:
     /**
     * @brief setFilterBeingDragged
@@ -257,6 +267,18 @@ class SIMPLView_UI : public QMainWindow
      */
     void resizeEvent ( QResizeEvent* event );
 
+    /**
+     * @brief activateBookmark
+     * @param filePath
+     * @param execute
+     */
+    void activateBookmark(const QString& filePath, bool execute);
+
+    /**
+     * @brief handlePipelineChanges
+     */
+    void handlePipelineChanges();
+
   protected slots:
     /**
      * @brief pipelineDidFinish
@@ -285,6 +307,13 @@ class SIMPLView_UI : public QMainWindow
     * @param hasErrors
     */
     void issuesTableHasErrors(bool hasErrors, int errCount, int warnCount);
+
+    /**
+     * @brief Update the FilterInputWidget based on the updated selection
+     * @param selected
+     * @param deselected
+     */
+    void filterSelectionChanged(const QItemSelection& selected, const QItemSelection& deselected);
 
     // Our Signals that we can emit custom for this class
   signals:
